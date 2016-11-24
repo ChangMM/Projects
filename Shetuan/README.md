a93e18e63589bfdb7ed5630243e07d2240cb865a
ChangMM <1769111903@qq.com>
2016-11-24 15:20:54 +0800

    添加社团空

---
+++ Shetuan/README.md
@@ -0,0 +1,2 @@
+#Society-Communicate
+###开发人员：前端：明明    后台：DuckKing
---
+++ Shetuan/php/login.php
@@ -0,0 +1,14 @@
+<?php
+ //    include "mysql.php";
+	// $result=mysql_query("select * from aso_tab where username='$_POST[username]' and psw='$_POST[password]'");
+	// $num=mysql_num_rows($result);
+	// if($num)
+	// {   session_start();
+	//     $_SESSION["soc_user"]="$_POST[username]";
+	// 	echo "1";
+	// 	}
+	// 	else echo "0";
+	session_start();
+	    $_SESSION["soc_user"]="$_POST[username]";
+		echo "1";
+?>
